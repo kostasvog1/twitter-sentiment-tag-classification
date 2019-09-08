27605afd1b79cbc66874b0d4e3724860e4df55e9
kostasvog1 <kostasvoglitsis@gmail.com>
2019-09-08 23:01:07 +0100

    fetch function,credentials,brain nb

---
+++ twitter_credentials.py
@@ -0,0 +1,7 @@
+## twitter credentials
+consumer_key = "TIOwLI2gQmSwBSrcSiEHiDGQb"
+consumer_secret = "4NxOqWgmXRMBF3Rpac2e6o4qWVziqZ03inurkJ8Ci5XoX9iitH"
+access_token = "290118385-KZCbPHpiGBPnYM3bO5D6uUAMioYUm0D0KhHpL5gb"
+access_token_secret = "NzrO8Av4MMio99YYjQaSVSsmuIf0Mo58TVJsKRvf68mAA"
+
+
